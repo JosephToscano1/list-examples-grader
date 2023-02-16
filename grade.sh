117cce3ed6f0ae8c512ba7ff27bedbf77c998495
Yshzi <83853443+Yshzi@users.noreply.github.com>
2023-02-16 10:43:17 -0800

    Andrew's edits

--- grade.sh
+++ grade.sh
@@ -3,3 +3,24 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+cp TestListExamples.java student-submission
+cd sudent-submission
+if [[ -f ListExample.java]]
+then 
+    echo 'Code Submitted'
+else
+    echo 'Code not found.'
+    exit
+fi 
+javac *.java
+javac -cp CPATH *.java
+if [[ $? == 0]]
+then
+    echo "Successfully compiled"
+else 
+    echo "Failed to compile"
+    exit
+fi
+java -cp CPATH org.junit.runner.JUnitCore TestListExamples > file.txt
+grep -c "Error" 
+
