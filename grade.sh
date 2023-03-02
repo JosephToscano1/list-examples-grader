65c403d6d13f83a423dc373706427272c8b9c913
Yshzi <83853443+Yshzi@users.noreply.github.com>
2023-03-02 10:06:55 -0800

    Made Tester

--- grade.sh
+++ grade.sh
@@ -1,11 +1,12 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
+rm -rf file.txt
 git clone $1 student-submission
 echo 'Finished cloning'
-cp TestListExamples.java student-submission
-cd sudent-submission
-if [[ -f ListExample.java]]
+
+cd student-submission
+if [[ -f ListExamples.java ]]
 then 
     echo 'Code Submitted'
 else
@@ -13,18 +14,24 @@ else
     exit
 fi 
 javac *.java
-javac -cp CPATH *.java
-if [[ $? == 0]]
+cd ..
+cp TestListExamples.java student-submission/
+javac -cp $CPATH student-submission/*.java
+cd student-submission
+if [[ $? == 0 ]]
 then
     echo "Successfully compiled"
 else 
     echo "Failed to compile"
     exit
 fi
-java -cp CPATH org.junit.runner.JUnitCore TestListExamples > file.txt
-if [[ grep -c "Error" == 0]]
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > file.txt
+fail=`grep -c "E" file.txt`
+if [[ $fail -eq 0 ]]
 then 
     echo "100%"
 else 
     echo "0% Try again."
+fi
+
 
